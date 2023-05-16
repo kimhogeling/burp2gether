0ca1d3ae58339edadf858a3218a636aac7853e46
Kim Hogeling <kim.hogeling@wps.de>
2023-05-16 16:25:15 +0200

    QR for share and logo animations

--- src/App.svelte
+++ src/App.svelte
@@ -21,6 +21,11 @@
       loggedIn = !!u;
     })
   }
+
+  let contentVisible = false;
+  setTimeout(() => {
+    contentVisible = true;
+  }, 3000);
 </script>
 
 <div class="content">
@@ -30,20 +35,29 @@
     <div class="card">This Browser does not support {audioOptions.join(', ')}  😭</div>
   {/if}
 
-  {#if supportedAudioMimeType}
-    {#if loadingUser}
-      <div class="card" transition:slide>
-        Checking if you're logged in..
-      </div>
-    {/if}
+  {#if contentVisible}
+    {#if supportedAudioMimeType}
+      {#if loadingUser}
+        <div class="card" transition:slide>
+          Checking if you're logged in..
+        </div>
+      {/if}
 
-    {#if !loadingUser && !user}
-      <Login bind:user/>
-    {/if}
+      {#if !loadingUser && !user}
+        <Login bind:user/>
+      {/if}
 
-    {#if loggedIn}
-      <Friends bind:friends {user}/>
-      <BurpList {user} {friends} {supportedAudioMimeType} />
+      {#if loggedIn}
+        <Friends bind:friends {user}/>
+        <BurpList {user} {friends} {supportedAudioMimeType}/>
+        <div class="share" transition:slide>
+          <p class="share-text">Share the burping experience:</p>
+          <img class="qr"
+               alt="QR Code to share the app"
+               src="https://firebasestorage.googleapis.com/v0/b/burp2gether.appspot.com/o/burp2gether-qr.jpeg?alt=media&token=0403eb1a-063d-462d-a1b5-1eb30e3933d6"
+               width="300"/>
+        </div>
+      {/if}
     {/if}
   {/if}
 </div>
@@ -55,5 +69,21 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  .share {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin: 3em 0 0;
+  }
+
+  .share-text {
+    color: white;
+    margin: 0 0 1em;
+  }
+
+  .qr {
+    border-radius: 0.5em;
   }
 </style>
--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,8 +1,28 @@
-<div class="logo">
-  <span>burp</span>
-  <span class="logo-colored-letter">2</span>
-  <span>gether</span>
-</div>
+<script>
+  import {fly} from 'svelte/transition';
+
+  let logoVisible = false;
+  let sloganVisible = false;
+
+  setTimeout(() => {
+    logoVisible = true
+  }, 1)
+  setTimeout(() => {
+    sloganVisible = true
+  }, 1)
+</script>
+{#if logoVisible}
+  <div class="logo" in:fly="{{ y: 300, duration: 2000 }}">
+    <span>burp</span>
+    <span class="logo-colored-letter">2</span>
+    <span>gether</span>
+  </div>
+{/if}
+{#if sloganVisible}
+  <div class="slogan" in:fly="{{ x: 200, duration: 700, delay: 1300 }}">
+    and <span class="logo-colored-letter">4</span> eachother!
+  </div>
+{/if}
 
 <style>
   .logo {
@@ -13,11 +33,28 @@
     color: var(--color-two);
     font-size: 3em;
     align-items: center;
-    margin: 0 0 .5em;
+    margin: 0;
   }
 
-  .logo-colored-letter {
+
+  .slogan {
+    font-family: Caveat, serif;
+    /*font-family: 'Luckiest Guy';*/
+    /*font-family: 'Press Start 2P';*/
+    color: var(--color-four);
+    font-size: 2.5rem;
+    text-align: center;
+    text-shadow: black 0 0 1px;
+    transform: rotate(-3deg);
+    margin: -.4em 0 .5em 0;
+  }
+
+  .logo .logo-colored-letter {
     transform: rotate(4deg) scale(1.5) translate(-.02em, .03em);
     color: var(--color-four);
+  }
+
+  .slogan .logo-colored-letter {
+    color: var(--color-two);
   }
 </style>
