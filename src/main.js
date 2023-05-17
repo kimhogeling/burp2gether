c1f13096d06b104778cafff0e2ea0fb411e72b52
Kim Hogeling <kim.hogeling@wps.de>
2023-05-17 08:20:22 +0200

    Extract firebase config

--- src/main.js
+++ src/main.js
@@ -4,25 +4,14 @@ import App from './App.svelte'
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {firebaseConfig} from "./firebase-config.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-};
-
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(firebaseApp);
+const analytics = getAnalytics(firebaseApp);
 
 const app = new App({
   target: document.getElementById('app')
