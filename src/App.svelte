ceb9eb4a9fcb60cadc888df26a804177e7f1a163
Kim Hogeling <kim.hogeling@wps.de>
2023-06-28 20:19:04 +0200

    fixes #14

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
   import {getAuth} from "firebase/auth";
   import BurpList from "./lib/BurpList.svelte";
   import Friends from "./lib/Friends.svelte";
-  import {slide} from 'svelte/transition';
+  import Share from "./lib/Share.svelte";
 
   let loggedIn = false;
   let user;
@@ -21,11 +21,6 @@
       loggedIn = !!u;
     })
   }
-
-  let contentVisible = false;
-  setTimeout(() => {
-    contentVisible = true;
-  }, 3000);
 </script>
 
 <div class="content">
@@ -35,30 +30,21 @@
     <div class="card">This Browser does not support {audioOptions.join(', ')}  😭</div>
   {/if}
 
-  {#if contentVisible}
-    {#if supportedAudioMimeType}
-      {#if loadingUser}
-        <div class="card" transition:slide>
-          Checking if you're logged in..
-        </div>
-      {/if}
-
-      {#if !loadingUser && !user}
-        <Login bind:user/>
-      {/if}
-
-      {#if loggedIn}
-        <Friends bind:friends {user}/>
-        <BurpList {user} {friends} {supportedAudioMimeType}/>
-        <div class="share" transition:slide>
-          <p class="share-text">Share the burping experience:</p>
-          <img class="qr"
-               alt="QR Code to share the app"
-               src="https://firebasestorage.googleapis.com/v0/b/burp2gether.appspot.com/o/burp2gether-qr.jpeg?alt=media&token=0403eb1a-063d-462d-a1b5-1eb30e3933d6"
-               width="300"/>
-        </div>
-      {/if}
+  {#if supportedAudioMimeType}
+    {#if loadingUser}
+      <div class="card">Looking for your login..</div>
     {/if}
+
+    {#if !loadingUser && !user}
+      <Login bind:user/>
+    {/if}
+
+    {#if loggedIn}
+      <Friends bind:friends {user}/>
+      <BurpList {user} {friends} {supportedAudioMimeType}/>
+    {/if}
+
+    <Share/>
   {/if}
 </div>
 
@@ -69,21 +55,5 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-  }
-
-  .share {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    margin: 3em 0 0;
-  }
-
-  .share-text {
-    color: white;
-    margin: 0 0 1em;
-  }
-
-  .qr {
-    border-radius: 0.5em;
   }
 </style>
---
+++ src/lib/Share.svelte
@@ -0,0 +1,41 @@
+<script>
+  import MdShare from 'svelte-icons/md/MdShare.svelte';
+
+  let showQrCode = false;
+</script>
+
+<div class="share">
+  <p class="share-text">Share the app:
+    {#if !showQrCode}
+      <button type="button" class="icon-button" on:click={() => { showQrCode = true}}>
+        <MdShare/>
+      </button>
+    {/if}
+  </p>
+
+  {#if showQrCode}
+    <img class="qr"
+         alt="QR Code to share the app"
+         src="https://firebasestorage.googleapis.com/v0/b/burp2gether.appspot.com/o/burp2gether-qr.jpeg?alt=media&token=0403eb1a-063d-462d-a1b5-1eb30e3933d6"
+         width="300"/>
+  {/if}
+</div>
+
+<style>
+  .share {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin: 3em 0 0;
+  }
+
+  .share-text {
+    color: white;
+    display: flex;
+    align-items: center;
+  }
+
+  .qr {
+    border-radius: 0.5em;
+  }
+</style>
